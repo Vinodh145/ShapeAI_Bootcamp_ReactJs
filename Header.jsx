6ed1369a342c3db66a0656756ab9fe6b085fe545
Vinodh145 <74820255+Vinodh145@users.noreply.github.com>
2021-06-27 12:08:08 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <header>
+        <h1> Shape AI Bootcamp Review</h1>
+        <p> -By VINODH KUMAR R</p>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
---
+++ Info.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1> JavaScript and ReactJs</h1>
+      <p>
+        {" "}
+        Amazing Bootcamp handled by Shaurya Sinha sir . He taught from basic
+        details about web then stepped into HTML , CSS ,JavaScript and
+        JavaScript Framework ReactJs . Finally this is my first project . Thanks
+        to ShapeAI and Shauryaq Sinha sir.{" "}
+      </p>
+    </div>
+  );
+}
+
+export default Info;
